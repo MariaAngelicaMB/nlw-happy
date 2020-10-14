88e70de793bb60c47e6f0b56f34758fd5ce4f7af
Maria Angélica de Matos <maria.angelicamatos@gmail.com>
2020-10-14 14:28:38 -0300

    excluindo

--- backend/src/routes.ts
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express'
-import OrphanagesController from './controllers/OrphanagesController'
-
-const routes = Router();
-
-routes.post('/orphanages', OrphanagesController.create);
-
-export default routes;
